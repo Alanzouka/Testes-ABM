9cfa59d5a289c915eea559a644510667e9a15347
Alanzouka <semata654321@gmail.com>
2026-06-22 12:21:19 -0300

    Atividade 1

---
+++ emailSuporteFeedback/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var nodemailer = require('nodemailer');
+
+var app = express();
+var server = http.Server(app);
+var port = 500;
+
+app.set("port", port);
+app.use()
