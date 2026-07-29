180f1e66c3787032acfc7ee4ca372a85b6ecfbd3
bianca911 <biancamoreirap25@gmail.com>
2026-07-29 08:56:43 -0300

    kjdjjdjd

--- hashseguros/teste-hash.js
+++ hashseguros/teste-hash.js
@@ -1,1 +1,21 @@
 const bcrypt = require('bcrypt');
+
+async function testarHash() {
+    const senhaOriginal = "123456";
+
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(senhaOriginal, saltRounds);
+    console.log("Senha original:", senhaOriginal);
+    console.log("Hash gerado:", hash);
+    
+    const senhaDigitadaCorreta = "123456";
+    const senhaDigitadaErrada = "senhaErrada";
+
+    const resultado1 = await bcrypt.compare(senhaDigitadaCorreta, hash);
+    const reseltado2 = await bcrypt.compare(senhaDigitadaErrada, hash);
+
+    console.log("Tá cero isso aí?", resultado1);
+    console.log("Tá errado não tá?", reseltado2);
+}
+
+testarHash();
