a2753fc22e7a23386c37e9969f86f980a808109b
MAYKOOOOO <maykon.salvador@escola.pr.gov.br>
2026-07-30 12:20:05 -0300

    TERMINEII UMA FUNÇAO DE LOGIN E COMEÇEI VISUALIZAR SENHAS ANTERIORES

--- testarsomething/database.js
+++ testarsomething/database.js
@@ -1,15 +1,16 @@
-import express from 'express'//pra conecta no servidor "api"
-import fs from 'fs'//negocio de armazenar arquivos 
-import path, { dirname } from 'path' //alguma coisa sobre se necessitar de um caminho absoluto pra url do banco
-import { fileURLToPath } from 'url'//o import usando o "path" comando de cima
- 
-const __filename = fileURLToPath(import.meta.url)// algo sobre importar do "meta url" o nome de algum arquivo
-const __dirname = path.dirname(__filename)// recebe o import de cima
+import express from 'express'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const app = express();//acho q define q o app ta no servidor
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express();
 const PORT = 3000;
 
-const dadosfilepath = path.join(dirname, 'dados.json'); //ele conecta os caminhos do database com os dados pra conseguir gerenciar
+const dadosfilepath = path.join(__dirname, 'dados.json'); 
 
-app.use(express.json());//pra conseguir ver os dados na tela?
-app.use(express.static('public'));//ele consegue manipular os dados publicos dentro de qualquer area do projeto
+app.use(express.json());
+app.use(express.static('public'));
+
