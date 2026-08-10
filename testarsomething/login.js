c0fa172afbe3a77b4a1e1d0046074633580b34bb
MAYKOOOOO <maykon.salvador@escola.pr.gov.br>
2026-08-10 10:54:45 -0300

    TERMINEI O SEGUNDO TESTE

--- testarsomething/login.js
+++ testarsomething/login.js
@@ -1,12 +1,14 @@
 const usuarios = require("./database");
-function login(nome, senha){
-const usuario = usuarios.find(u => u.nome === nome && u.senha === senha);
-if (!usuario){
-    return "usuario ou senha incorretos.";
+
+function login(nome, senha) {
+    const usuario = usuarios.find(u => u.nome === nome && u.senha === senha);
+    if (!usuario) {
+        return "usuario ou senha incorretos.";
+    }
+    if (!usuario.validado) {
+        return "conta ainda nao validada.";
+    }
+    return "login realizado com sucesso!";
 }
-if (!usuario.validado){
-    return "conta ainda nao validada.";
-}
-return "login realizado com sucesso!";
-}
+
 module.exports = login;
--- testarsomething/server.js
+++ testarsomething/server.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const cadastrar = require("./cadastro");
-const validados = require("./validaçao");
+const validar = require("./validacao");
 const login = require("./login");
 
 const app = express();
 app.use(express.json());
-app.post("/cadastro",(req, res) =>{
-    const {nome, senha, confirmarsenha} = req.body;
+
+app.post("/cadastro", (req, res) => {
+    const { nome, senha, confirmarsenha } = req.body;
+    res.send(cadastrar(nome, senha, confirmarsenha));
+});
+
+app.post("/validacao", (req, res) => {
+    const { nome } = req.body;
     res.send(validar(nome));
 });
 
-app.post("/validacao", (req, res) =>{
-    const {nome} = req.body;
-    res.send(validar(nome));
-});
-
-app.post("/login", (req, res) =>{
-    const{nome, senha} = req.body;
+app.post("/login", (req, res) => {
+    const { nome, senha } = req.body;
     res.send(login(nome, senha));
 });
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log("servidor rodando em http://localhost:3000");
 });
